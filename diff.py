fd99eab168bcf9809020a5535a9b1c9a99b80e3d
zhaotong7 <zhaot@usc.edu>
2016-06-08 08:41:30 -0700

    Update diff.py

--- diff.py
+++ diff.py
@@ -1,20 +1,27 @@
 import cv2
 import math
+import numpy as np
 
 img1 = cv2.imread('img1.png')
 img2 = cv2.imread('img2.png')
 res = img2
 mask = img2
-diff = img2 - img1
-cv2.imwrite('diff.png',diff)
+diff = cv2.absdiff(img2, img1)
+
+# cv2.imwrite('diff.png',diff)
 
 rows, cols, channels = img1.shape
 
+# compare each piexl with threhold
 for i in range(0,rows):
 	for j in range(0, cols):
-		# dist = math.sqrt(int(diff[i, j, 0])*int(diff[i, j, 0])+int(diff[i, j, 1])*int(diff[i, j, 1])+int(diff[i, j, 2])*int(diff[i, j, 2]))
-		if int(diff[i, j, 0]) < 1:
+		dist = math.sqrt(int(diff[i, j, 0])*int(diff[i, j, 0])+int(diff[i, j, 1])*int(diff[i, j, 1])+int(diff[i, j, 2])*int(diff[i, j, 2]))
+		if dist < 50:
 			mask[i,j] = [0,0,0]		
-print diff[1,2]
-cv2.imwrite('222.png', mask)
+
+# cv2.imwrite('mask.png', mask)
+
+# handle noise
+kernel = np.ones((3,3),np.uint8)
+opening = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
 
